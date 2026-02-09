8ecb93c1101e092d6e237e587cee12957c990eaf
Ramakrishna <jramakrishna122@gmail.com>
2026-02-09 20:22:20 +0530

    Create config/database.php with database configuration

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => 'localhost', // Database host
+    'database' => 'your_database_name', // Database name
+    'username' => 'your_username', // Database username
+    'password' => 'your_password', // Database password
+    'charset' => 'utf8', // Database charset
+    'collation' => 'utf8_unicode_ci', // Database collation
+    'prefix' => '', // Table prefix
+];
